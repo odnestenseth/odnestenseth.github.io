65a098621d67b0f1ef4476dada44397d1fa4bbf5
Odne <odne.stenseth@gmail.com>
2022-10-23 12:18:41 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,74 @@
-$(document).ready(function () {
-    $("#contactMeButton").click(function () {
-        $(".overlay").css("visibility", "visible");
-        $(".overlay").css("opacity", "1");
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    const tabButtons = [].slice.call(document.querySelectorAll('ul.clean-nav li a.button'));
 
-    $("#closeContactForm").click(function () {
-        $(".overlay").css("opacity", "0");
-        setTimeout(function () {
-            $(".overlay").css("visibility", "hidden");
-        }, 500);
+    tabButtons.map(button => {
+        button.addEventListener('click', e => {
+            document.getElementById("contact").style.display = "block"
+            const currentActiveButton = document.querySelector('li a.active.button')
+            if (currentActiveButton) currentActiveButton.classList.remove('active')
+            button.classList.add('active')
 
-    });
+            const currentActivePane = document.querySelector('.tab-pane.active')
+            if (currentActivePane) currentActivePane.classList.remove('active')
 
-    $("#contactForm").on("submit", function (e) {
+            const newActivePane = document.querySelector("[id='" + button.getAttribute('paneid') + "']")
+            newActivePane.classList.add('active')
+
+            const fadeIns = document.getElementsByClassName("fade-in")
+            for(const fadeIn of fadeIns) fadeIn.classList.remove("is-visible")
+
+            checkFadeIns()
+        })
+    })
+
+    const checkFadeIns = () => {
+        const fadeIns = document.getElementsByClassName("fade-in")
+        for (const fadeIn of fadeIns) {
+            const elementTop = fadeIn.offsetTop
+            const elementBottom = fadeIn.offsetTop + fadeIn.offsetHeight
+            const screenTop = window.scrollY
+            const screenBottom = window.scrollY + window.innerHeight;
+
+            if ((screenBottom > elementTop) && (screenTop < elementBottom) && !fadeIn.classList.contains('is-visible')) {
+                fadeIn.classList.add('is-visible');
+            }
+
+        }
+    }
+
+    window.addEventListener("scroll", e => { checkFadeIns() })
+
+    const overlay = document.getElementById("overlay")
+    document.getElementById("contactMeButton").addEventListener("click", e => { overlay.style.display = "block" })
+    document.getElementById("closeContactForm").addEventListener("click", e => { overlay.style.display = "none" })
+
+    document.getElementById("contactForm").addEventListener("submit", e => {
         e.preventDefault();
 
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var message = $("#message").val();
+        const name = document.getElementById("name").value
+        const email = document.getElementById("email").value
+        const message = document.getElementById("message").value
 
-        $.ajax({
-            url: "https://formspree.io/f/mgerrqwr",
+        fetch("https://formspree.io/f/mgerrqwr", {
             method: "POST",
-            data: {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
                 name: name,
                 _replyto: email,
                 email: email,
                 message: message,
                 _subject: "Contact Form Submission from " + name
-            },
-            dataType: "json"
+            })
+        })
 
-        });
-
-        $("#contactForm :input").prop("disabled", true);
-        $("#contactForm").css("display", "none");
-        $("#thankYou").css("display", "block");
+        //$("#contactForm :input").prop("disabled", true);
+        document.getElementById("contactForm").style.display = "none"
+        document.getElementById("thankYou").style.display = "block"
     });
-})
+
+
+
+}, false)
