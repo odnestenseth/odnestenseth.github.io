e426878e9e5c80cd354f7d47722904bb86465410
odnestenseth <33007635+odnestenseth@users.noreply.github.com>
2026-05-22 13:18:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -113,30 +113,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const generateCard = item => {
 	const card = document.createElement('div')
-	card.className = 'mb-2 p-4 rounded-lg border border-gray-700 shadow-sm flex justify-between gap-6'
+	card.className = 'mb-2 p-4 rounded-lg border border-gray-700 shadow-sm flex flex-col justify-between gap-6'
 	card.innerHTML = `
-<div class="flex-1">
-    <h3 class="text-3xl font-bold text-[#f43f5e]">${item.title}</h3>
-    <p class="text-xl font-semibold">${item.subtitle}</p>
-    <p class="text whitespace-pre-wrap mt-2 text-gray-400">${item.description}</p>
-    
-    <div class="flex flex-wrap gap-x-4 gap-y-1 pt-2 text">
-    <span class="flex items-center">
-        <svg class="w-3.5 h-3.5 mr-1 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-        ${item.dateTimeString}
-    </span>
-    <span class="flex items-center">
-        <svg class="w-3.5 h-3.5 mr-1 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-        ${item.googleMapsURL ? `<a href=${item.googleMapsURL}>${item.location}</a>` : item.location}
-    </span>
+    <div class="flex">
+        <div class="flex-1">
+            <h3 class="text-3xl font-bold text-[#f43f5e]">${item.title}</h3>
+            <p class="text-xl font-semibold">${item.subtitle}</p>
+        </div>
+        <div class="text-2xl bg-[#bef264] text-[#064e3b] font-semibold p-4 rounded text-center">
+            ${item.price}
+        </div>
     </div>
-</div>
 
-<div class="flex items-center h-full">
-    <div class="text-2xl bg-[#bef264] text-[#064e3b] font-semibold p-4 rounded text-center">
-        ${item.price}
+    <div class="flex flex-wrap gap-x-4 gap-y-1 pt-2 text">
+	    <p class="text whitespace-pre-wrap mt-2 text-gray-400">${item.description}</p>
+        <span class="flex items-center">
+            <svg class="w-3.5 h-3.5 mr-1 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            ${item.dateTimeString}
+        </span>
+        <span class="flex items-center">
+            <svg class="w-3.5 h-3.5 mr-1 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+            ${item.googleMapsURL ? `<a href=${item.googleMapsURL}>${item.location}</a>` : item.location}
+        </span>
     </div>
-</div>`
+`
 
 	return card
 }
@@ -153,7 +153,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 	if (!dropIns || dropIns.length === 0) {
 		document.getElementById('drop-ins-collection-no-class-msg').style.display = 'block'
-        document.getElementById('drop-ins-collection').style.display = 'none'
+		document.getElementById('drop-ins-collection').style.display = 'none'
 	} else {
 		const dropInsSection = document.getElementById('drop-ins-collection')
 		dropIns.forEach(item => {
@@ -168,7 +168,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 	if (!shortClasses || shortClasses.length === 0) {
 		document.getElementById('short-term-collection-no-class-msg').style.display = 'block'
-        document.getElementById('short-courses-collection').style.display = 'none'
+		document.getElementById('short-courses-collection').style.display = 'none'
 	} else {
 		const shortCoursesSection = document.getElementById('short-courses-collection')
 		shortClasses.forEach(item => {
